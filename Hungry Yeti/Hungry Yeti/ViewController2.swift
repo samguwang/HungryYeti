9dad37b6cf86a63f6b0ad15c4ddba49208191e91
samguwang <samwang@cs.utexas.edu>
2015-06-17 21:30:38 -0500

    added buttons

---
+++ Hungry Yeti/Hungry Yeti/ViewController2.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController2.swift
+//  hungryyeti
+//
+//  Created by Sam Wang on 6/16/15.
+//  Copyright (c) 2015 eightAM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+    
+    
+    @IBOutlet weak var YelpImage: UIImageView!
+    
+    @IBOutlet weak var starIcons: UIImageView!
+    
+    @IBOutlet weak var refreshSearch: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
