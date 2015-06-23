fec915625202260724d70d5c24b1a465fa8510a6
Sam Wang <sam.wang2@target.com>
2015-06-23 13:47:48 -0500

    added mapkit

--- Hungry Yeti/Hungry Yeti/businessInfoView.swift
+++ Hungry Yeti/Hungry Yeti/businessInfoView.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import MapKit
 import CoreLocation
 
 
@@ -19,6 +20,8 @@ class businessInfoView: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var businessName: UILabel!
    
     
+    @IBOutlet weak var starImage: UIImageView!
+    @IBOutlet weak var map: MKMapView!
     
     
     var businesses: [Business]!
@@ -26,12 +29,13 @@ class businessInfoView: UIViewController, CLLocationManagerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //setup userlocation settings
         self.loading.startAnimating()
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestAlwaysAuthorization()
         locationManager.startUpdatingLocation()
-        println("starting")
+        println("starting view controller2")
         
     }
     
@@ -48,43 +52,75 @@ class businessInfoView: UIViewController, CLLocationManagerDelegate {
     }
     
     //after grabbing a business, display information
-    func updateInfo(selected: Business){
-        self.loading.hidden = true
-        self.loading.startAnimating()
-        let name = selected.name
+    func updateInfo(selected: Business, coord: CLLocationCoordinate2D){
+        let name = selected.name!
         businessName.text = name
+        
+        println("calling updateinfo")
+        
+        //mapkit setup
+        var span = MKCoordinateSpanMake(0.01, 0.01)
+        var region = MKCoordinateRegion(center: coord, span: span)
+        map.setRegion(region, animated: true)
+        var annotation = MKPointAnnotation()
+        annotation.coordinate = coord
+        annotation.title = name
+        map.addAnnotation(annotation)
+        let data = NSData(contentsOfURL: selected.ratingImageURL!)
+        starImage.image = UIImage(data: data!)
+        self.loading.hidden = true
+        self.loading.stopAnimating()
+        
         
     }
     
+    //function used to call make API request to Yelp servers
+    func callAPI(cord: CLLocation){
+        let te = "Food"
+        Business.searchByLocationRatingDistance(te, limit: 20, Lat: cord.coordinate.latitude, Long: cord.coordinate.longitude, sort: 0, categories: "restaurants", radius_filter: 2000, completion: { (businesses: [Business]!, error: NSError!) -> Void in
+            self.businesses = businesses
+            println("calling callAPI function")
+            let top = businesses.endIndex
+            let ran = self.randRange(0, upper: 19)
+            var selectedBusiness = businesses[ran]
+            let businame = selectedBusiness.name!
+            println(businame)
+            println(selectedBusiness.ratingImageURL)
+            self.updateInfo(selectedBusiness, coord: cord.coordinate)
+        })
+    }
     
+    func passonLocation(loc: CLLocation){
+        println("calling passonlocation")
+        callAPI(loc)
+    }
+    
+    //locationmanager delegate function from CLLocationManager, called when user location updated
     func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
-        
+        var i = 0
+        println("callign locationmanager")
+        println(locations[0])
         //store location in location variable of type CLLocation
         var location:CLLocation = locations[locations.count-1] as! CLLocation
         
         //check for location accuracy and stop when accurate enough
         if (location.horizontalAccuracy > 0) {
             self.locationManager.stopUpdatingLocation()
-            println(location.coordinate)
+            println(location.coordinate.latitude)
+            println(location.coordinate.longitude)
             
         }
-        let te = "Food"
-        Business.searchByLocationRatingDistance(te, limit: 20, Lat: location.coordinate.latitude, Long: location.coordinate.longitude, sort: 0, categories: "restaurants", radius_filter: 2000, completion: { (businesses: [Business]!, error: NSError!) -> Void in
-            self.businesses = businesses
-            let top = businesses.endIndex
-            let ran = self.randRange(0, upper: 19)
-            var selectedBusiness = businesses[ran]
-            let businame = selectedBusiness.name
-            println(businame)
-            self.updateInfo(selectedBusiness)
-            
-        })
+        println(location.timestamp)
+        if(i == 0){
+        passonLocation(location)
+        }
+        
+        ++i
+        println(i)
         
     }
     
-    
-    
-    
+    //locationManager delegate function if failure
     func locationManager(manager: CLLocationManager!, didFailWithError error: NSError!) {
         println(error)
     }
